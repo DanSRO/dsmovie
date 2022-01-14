ef66bf2214d6b0617ee2df9626bc6aec81bdc098
daniel dantas <dan_dantas@hotmail.com>
2022-01-13 22:21:34 -0300

    Add Find Movies

--- backend/src/main/java/com/devsuperior/dsmovie/Entities/ScorePK.java
+++ backend/src/main/java/com/devsuperior/dsmovie/Entities/ScorePK.java
@@ -7,8 +7,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 @Embeddable
-public class ScorePK implements Serializable{
-    private static final long serialVersionUID=1L;
+public class ScorePK implements Serializable{    
     
     @ManyToOne
     @JoinColumn(name="movie_id")
